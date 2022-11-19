12d195a12de7505e648e34482dd4cdca9083504e
rowjay007 <rowlives@gmail.com>
2022-11-19 13:53:27 +0100

    add header topbar nav

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import "./App.css";
 import { ColorModeContext, useMode } from "./context/theme";
+import Header from "./container/window/Header";
 
 function App() {
-
-  const [theme, colorMode] = useMode()
+  const [theme, colorMode] = useMode();
   return (
     <ColorModeContext.Provider value={colorMode}>
       <ThemeProvider theme={theme}>
-        <CssBaseline/>
+        <CssBaseline />
         <div>
-          <main className="content"></main>
+          <main className="content">
+            <Header />
+          </main>
         </div>
       </ThemeProvider>
     </ColorModeContext.Provider>
--- src/container/window/Header.jsx
+++ src/container/window/Header.jsx
@@ -1,9 +1,52 @@
-import React from 'react'
+// @ts-nocheck
+import { Box, IconButton, useTheme, InputBase } from "@mui/material";
+import React, { useContext } from "react";
+import { ColorModeContext, tokens } from "../../context/theme";
+import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
+import SearchIcon from "@mui/icons-material/Search";
 
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
-}
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const colorMode = useContext(ColorModeContext);
 
-export default Header
+  return (
+    <Box display="flex" justifyContent="space-between" p={2}>
+      <Box
+        display="flex"
+        backgroundColor={colors.primary[400]}
+        borderRadius="3px"
+      >
+        <InputBase sx={{ ml: 2, flex: 1 }} placeholder="Search" />
+        <IconButton>
+          {" "}
+          <SearchIcon />
+        </IconButton>
+      </Box>
+      <Box>
+        <IconButton onClick={colorMode.toggleColorMode}>
+          {theme.palette.mode === "dark" ? (
+            <DarkModeOutlinedIcon />
+          ) : (
+            <LightModeOutlinedIcon />
+          )}
+        </IconButton>
+        <IconButton>
+          <NotificationsOutlinedIcon />
+        </IconButton>
+        <IconButton>
+          <SettingsOutlinedIcon />
+        </IconButton>
+        <IconButton>
+          <PersonOutlinedIcon />
+        </IconButton>
+      </Box>
+    </Box>
+  );
+};
+
+export default Header;
