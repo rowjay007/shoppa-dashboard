040028ba1a185bef0e16fc59623a2a5446e5edb8
rowjay007 <rowlives@gmail.com>
2022-11-19 14:42:56 +0100

    react-router-routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 // @ts-nocheck
+import { Route, Routes } from "react-router-dom";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import "./App.css";
 import { ColorModeContext, useMode } from "./context/theme";
 import Header from "./container/window/Header";
+import Sidebar from "./container/window/Sidebar";
+import Dashboard from "./container/dashboard";
+// import Team from "./container/team";
+// import Invoice from "./container/invoice";
+// import Contacts from "./container/contacts";
+// import Bar from "./container/bar";
+// import Form from "./container/form";
+// import Line from "./container/line";
+// import Pie from "./container/pie";
+// import Geography from "./container/geography";
+// import Calender from "./container/calender";
+// import Faq from "./container/faq";
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -10,9 +23,24 @@ function App() {
     <ColorModeContext.Provider value={colorMode}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
-        <div>
+        <div className="app">
+          <Sidebar />
           <main className="content">
             <Header />
+
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              {/* <Route path='/team' element={<Team />} /> */}
+              {/* <Route path='/contacts' element={<Contacts />} /> */}
+              {/* <Route path='/invoice' element={<Invoice />} /> */}
+              {/* <Route path='/form' element={<Form />} /> */}
+              {/* <Route path='/bar' element={<Bar />} /> */}
+              {/* <Route path='/pie' element={<Pie />} /> */}
+              {/* <Route path='/line' element={<Line />} /> */}
+              {/* <Route path='/geography' element={<Geography />} /> */}
+              {/* <Route path='/calender' element={<Calender />} /> */}
+              {/* <Route path='/faq' element={<Faq />} /> */}
+            </Routes>
           </main>
         </div>
       </ThemeProvider>
