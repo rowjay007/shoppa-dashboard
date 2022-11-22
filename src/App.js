1804f1e0df2ccd6698b38ce159d161fb524979d9
rowjay007 <rowlives@gmail.com>
2022-11-22 15:35:45 +0100

    add resuable header components

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import { Route, Routes } from "react-router-dom";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { ColorModeContext, useMode } from "./context/theme";
-import Header from "./container/window/Header";
+import Topbar from "./container/window/Topbar";
 import Sidebar from "./container/window/Sidebar";
 import Dashboard from "./container/dashboard";
 import { useState } from "react";
@@ -27,7 +27,7 @@ function App() {
         <div className="app">
           <Sidebar isSidebar={isSidebar} />
           <main className="content">
-            <Header setIsSidebar={setIsSidebar} />
+            <Topbar setIsSidebar={setIsSidebar} />
             <Routes>
               <Route path="/" element={<Dashboard />} />
               {/* <Route path='/team' element={<Team />} /> */}
---
+++ src/components/Header.jsx
@@ -0,0 +1,25 @@
+import { Box, Typography, useTheme } from "@mui/material";
+import React from "react";
+import { tokens } from "../context/theme";
+
+const Header = ({ title, subtitle }) => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  return (
+    <Box mb="30px">
+      <Typography
+        variant="h2"
+        color={colors.grey[100]}
+        fontWeight="bold"
+        sx={{ mb: "5px" }}
+      >
+        {title}
+      </Typography>
+      <Typography variant="h6" color={colors.blueAccent[400]}>
+        {subtitle}
+      </Typography>
+    </Box>
+  );
+};
+
+export default Header;
