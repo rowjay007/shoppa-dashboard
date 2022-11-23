dea07c1bf5af0b14d4770518bae162cc0efcc5f7
rowjay007 <rowlives@gmail.com>
2022-11-23 09:32:00 +0100

    styling team table with mui

--- src/container/team/index.jsx
+++ src/container/team/index.jsx
@@ -13,68 +13,86 @@ import { mockDataTeam } from "../../data/mockData";
 const Team = () => {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
- const columns = [
-   { field: "id", headerName: "ID" },
-   {
-     field: "name",
-     headerName: "Name",
-     flex: 1,
-     cellClassName: "name-column--cell",
-   },
-   {
-     field: "age",
-     headerName: "Age",
-     type: "number",
-     headerAlign: "left",
-     align: "left",
-   },
-   {
-     field: "phone",
-     headerName: "Phone Number",
-     flex: 1,
-   },
-   {
-     field: "email",
-     headerName: "Email",
-     flex: 1,
-   },
-   {
-     field: "accessLevel",
-     headerName: "Access Level",
-     flex: 1,
-     renderCell: ({ row: { access } }) => {
-       return (
-         <Box
-           width="60%"
-           m="0 auto"
-           p="5px"
-           display="flex"
-           justifyContent="center"
-           backgroundColor={
-             access === "admin"
-               ? colors.blueAccent[600]
-               : access === "manager"
-               ? colors.blueAccent[700]
-               : colors.blueAccent[700]
-           }
-           borderRadius="4px"
-         >
-           {access === "admin" && <AdminPanelSettingsOutlinedIcon />}
-           {access === "manager" && <SecurityOutlinedIcon />}
-           {access === "user" && <LockOpenOutlinedIcon />}
-           <Typography color={colors.grey[100]} sx={{ ml: "5px" }}>
-             {access}
-           </Typography>
-         </Box>
-       );
-     },
-   },
- ];
+  const columns = [
+    { field: "id", headerName: "ID" },
+    {
+      field: "name",
+      headerName: "Name",
+      flex: 1,
+      cellClassName: "name-column--cell",
+    },
+    {
+      field: "age",
+      headerName: "Age",
+      type: "number",
+      headerAlign: "left",
+      align: "left",
+    },
+    {
+      field: "phone",
+      headerName: "Phone Number",
+      flex: 1,
+    },
+    {
+      field: "email",
+      headerName: "Email",
+      flex: 1,
+    },
+    {
+      field: "accessLevel",
+      headerName: "Access Level",
+      flex: 1,
+      renderCell: ({ row: { access } }) => {
+        return (
+          <Box
+            width="60%"
+            m="0 auto"
+            p="5px"
+            display="flex"
+            justifyContent="center"
+            backgroundColor={
+              access === "admin"
+                ? colors.blueAccent[600]
+                : access === "manager"
+                ? colors.blueAccent[700]
+                : colors.blueAccent[700]
+            }
+            borderRadius="4px"
+          >
+            {access === "admin" && <AdminPanelSettingsOutlinedIcon />}
+            {access === "manager" && <SecurityOutlinedIcon />}
+            {access === "user" && <LockOpenOutlinedIcon />}
+            <Typography color={colors.grey[100]} sx={{ ml: "5px" }}>
+              {access}
+            </Typography>
+          </Box>
+        );
+      },
+    },
+  ];
 
   return (
     <Box>
       <Header title="TEAM" subtitle="Managing the Team Members" />
-      <Box m="40px 0 0 0" height="75vh">
+      <Box
+        m="40px 0 0 0"
+        height="75vh "
+        sx={{
+          "& .MuiDataGrid-root": { border: "none" },
+          "& .name-column--cell": { color: colors.blueAccent[300] },
+          "& .MuiDataGrid-columnHeaders": {
+            color: colors.blueAccent[700],
+            borderBottom: "none",
+          },
+          "& .MuiDataGrid-virtualScroller": {
+            backgroundColor: colors.primary[400],
+          },
+          "& .MuiDataGrid-footerContainer": {
+            borderTop: "none",
+            backgroundColor: colors.blueAccent[700],
+          },
+        }}
+      >
         <DataGrid rows={mockDataTeam} columns={columns} />
       </Box>
     </Box>
