98471fe5e5c6fbd1ec78b76c79b4fd74cc09999d
rowjay007 <rowlives@gmail.com>
2022-11-30 10:51:29 +0100

    add line container

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import Contacts from "./container/contacts";
 import Bar from "./container/bar";
 import Form from "./container/form";
 import Calendar from "./container/calendar";
-// import Line from "./container/line";
+import Line from "./container/line";
 import Pie from "./container/pie";
 import Geography from "./container/geography";
 import Faq from "./container/faq";
@@ -36,7 +36,7 @@ function App() {
               <Route path="/form" element={<Form />} />
               <Route path='/bar' element={<Bar />} />
               <Route path='/pie' element={<Pie />} />
-              {/* <Route path='/line' element={<Line />} /> */}
+              <Route path='/line' element={<Line />} />
               <Route path='/geography' element={<Geography />} />
               <Route path="/calendar" element={<Calendar />} />
               <Route path='/faq' element={<Faq />} />
